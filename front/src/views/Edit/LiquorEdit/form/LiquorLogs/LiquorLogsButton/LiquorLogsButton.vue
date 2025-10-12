7ff5938ffb1ffd66485de6942c24cbeaec812605
t-moriyama-r <ms.shopper.tm@gmail.com>
2025-10-12 19:07:11 +0900

    カテゴリも同様のデザインにする

--- front/src/views/Edit/LiquorEdit/form/LiquorLogs/LiquorLogsButton/LiquorLogsButton.vue
+++ front/src/views/Edit/LiquorEdit/form/LiquorLogs/LiquorLogsButton/LiquorLogsButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <CommonButton>過去のバージョンを選択</CommonButton>
+  <CommonButton>過去のバージョンを選択(TODO)</CommonButton>
 </template>
 <script setup lang="ts">
 import CommonButton from '@/components/parts/common/CommonButton/CommonButton.vue';
