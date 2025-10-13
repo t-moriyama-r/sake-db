606ad52da19ac5be372e9fedd78da6c404a3fcc9
t-moriyama-r <ms.shopper.tm@gmail.com>
2025-10-14 01:09:39 +0900

    空のカテゴリの場合文言を表示する

---
+++ front/src/views/Discovery/NarrowDowns/EmptyCategory.vue
@@ -0,0 +1,5 @@
+<template>登録してね</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- front/src/views/Discovery/NarrowDowns/FromCategory.vue
+++ front/src/views/Discovery/NarrowDowns/FromCategory.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="random-pickup-area">
+  <div id="random-pickup-area" v-if="liquors.length > 0">
     <CardContainer
       columns="repeat(auto-fill, minmax(100px, 1fr)"
       gap="0.5em"
@@ -8,12 +8,14 @@
       <LiquorCard v-for="liquor in liquors" :liquor="liquor" :key="liquor.id" />
     </CardContainer>
   </div>
+  <EmptyCategory v-else />
 </template>
 
 <script setup lang="ts">
 import LiquorCard from '@/components/blocks/cards/LiquorCard.vue';
 import CardContainer from '@/components/parts/common/CardContainer.vue';
 import type { Liquor } from '@/graphQL/Index/random';
+import EmptyCategory from '@/views/Discovery/NarrowDowns/EmptyCategory.vue';
 
 interface Props {
   liquors: Liquor[];
